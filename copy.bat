9ef9c3c558ce2a4f5c1f61774c099b00a43780b2
Zyntex <komocsinlaci@gmail.com>
2023-10-08 23:29:49 +0200

    fix copy.bat

--- copy.bat
+++ copy.bat
@@ -2,4 +2,4 @@ SET "binary=wormhole.dll"
 SET "src=..\bin\%binary%"
 SET "dest=D:\SteamLibrary\SteamApps\common"
 
-copy /Y "%src%" "%dest%\Portal 2\%binary%"
+REM copy /Y "%src%" "%dest%\Portal 2\%binary%"
