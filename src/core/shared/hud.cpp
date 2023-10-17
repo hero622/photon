77163618686653f55f800f9343ba2d9a69ff20ce
Zyntex <komocsinlaci@gmail.com>
2023-10-18 00:27:05 +0200

    fuck iterators

--- src/core/shared/hud.cpp
+++ src/core/shared/hud.cpp
@@ -2,8 +2,6 @@
 
 #include "core/huds/huds.h"
 
-#include <iterator>
-
 void c_huds::reg(wh_api::i_hud *hud) {
 	huds::huds.push_back(hud);
 }
@@ -11,13 +9,15 @@ void c_huds::reg(wh_api::i_thud *thud) {
 	huds::thuds.push_back(thud);
 }
 void c_huds::unreg(wh_api::i_hud *hud) {
-	std::vector<wh_api::i_hud *>::iterator it = std::find<std::vector<wh_api::i_hud *>::iterator, wh_api::i_hud *>(huds::huds.begin(), huds::huds.end(), hud);
-	if (it != huds::huds.end())
-		huds::huds.erase(it);
+	for (int i = 0; i < huds::huds.size(); ++i) {
+		if (huds::huds[i] == hud)
+			huds::huds.erase(huds::huds.begin() + i);
+	}
 	// huds::huds.erase(hud);
 }
 void c_huds::unreg(wh_api::i_thud *thud) {
-	std::vector<wh_api::i_thud *>::iterator it = std::find<std::vector<wh_api::i_thud *>::iterator, wh_api::i_thud *>(huds::thuds.begin(), huds::thuds.end(), thud);
-	if (it != huds::thuds.end())
-		huds::thuds.erase(it);
+	for (int i = 0; i < huds::thuds.size(); ++i) {
+		if (huds::thuds[i] == thud)
+			huds::thuds.erase(huds::thuds.begin() + i);
+	}
 }
