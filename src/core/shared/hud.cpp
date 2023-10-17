5a2a2c287862f1fa5d1efcea26105df6588e0c7d
Zyntex <komocsinlaci@gmail.com>
2023-10-18 00:18:43 +0200

    fix linux build ?

--- src/core/shared/hud.cpp
+++ src/core/shared/hud.cpp
@@ -11,13 +11,13 @@ void c_huds::reg(wh_api::i_thud *thud) {
 	huds::thuds.push_back(thud);
 }
 void c_huds::unreg(wh_api::i_hud *hud) {
-	std::vector<wh_api::i_hud *>::iterator it = std::find(huds::huds.begin(), huds::huds.end(), hud);
+	std::vector<wh_api::i_hud *>::iterator it = std::find<std::vector<wh_api::i_hud *>::iterator, wh_api::i_hud *>(huds::huds.begin(), huds::huds.end(), hud);
 	if (it != huds::huds.end())
 		huds::huds.erase(it);
 	// huds::huds.erase(hud);
 }
 void c_huds::unreg(wh_api::i_thud *thud) {
-	std::vector<wh_api::i_thud *>::iterator it = std::find(huds::thuds.begin(), huds::thuds.end(), thud);
+	std::vector<wh_api::i_thud *>::iterator it = std::find<std::vector<wh_api::i_thud *>::iterator, wh_api::i_thud *>(huds::thuds.begin(), huds::thuds.end(), thud);
 	if (it != huds::thuds.end())
 		huds::thuds.erase(it);
 }
