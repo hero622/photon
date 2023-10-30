601f814bbd0721a35ca811f080489d9e347c2802
Zyntex <komocsinlaci@gmail.com>
2023-10-30 12:34:57 +0100

    fix type error

--- src/core/shared/hud.cpp
+++ src/core/shared/hud.cpp
@@ -3,6 +3,8 @@
 #include "core/huds/huds.h"
 #include "wormhole-sdk/wormhole.h"
 
+#include <cstddef>
+
 void c_huds::reg( wh_api::i_hud *hud ) {
 	huds::huds.push_back( hud );
 }
@@ -10,13 +12,13 @@ void c_huds::reg( wh_api::i_thud *thud ) {
 	huds::thuds.push_back( thud );
 }
 void c_huds::unreg( wh_api::i_hud *hud ) {
-	for ( int i = 0; i < huds::huds.size( ); ++i ) {
+	for ( std::size_t i = 0; i < huds::huds.size( ); ++i ) {
 		if ( huds::huds[ i ] == hud )
 			huds::huds.erase( huds::huds.begin( ) + i );
 	}
 }
 void c_huds::unreg( wh_api::i_thud *thud ) {
-	for ( int i = 0; i < huds::thuds.size( ); ++i ) {
+	for ( std::size_t i = 0; i < huds::thuds.size( ); ++i ) {
 		if ( huds::thuds[ i ] == thud )
 			huds::thuds.erase( huds::thuds.begin( ) + i );
 	}
